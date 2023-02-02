13d69ab2214b8798e6be5894366526e6a422ab06
Sophie Lee <2020soop@gmail.com>
2023-02-02 10:34:05 +0800

    [Feat] first commit, setting container

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '32px',
+        md: '24px',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
